e543679853f26915a432ee47809cf3f8c4c18560
Logan Michalicek <logan.michalicek@gmail.com>
2021-12-11 13:13:51 -0700

    Joining game session now starts with existing players

--- client/src/gameUtil.js
+++ client/src/gameUtil.js
@@ -2,22 +2,40 @@ if (!module || !module.exports) {
   module.exports = {};
 }
 
-const movementController = function (cursors, player) {
-  if (cursors.left.isDown) {
+const movementController = function (cursors, player, io) {
+  function communicate() {
+    io.emit('movement', {
+      x: player.x,
+      y: player.y
+    });
+  }
+  if (cursors.left.isDown && cursors.right.isDown) {
+    player.setVelocityX(0);
+    player.anims.play('turn');
+  } else if (cursors.left.isDown) {
     player.setVelocityX(-160);
     player.anims.play('left', true);
+    communicate();
   }
   else if (cursors.right.isDown) {
-      player.setVelocityX(160);
-      player.anims.play('right', true);
-  }
-  else {
-      player.setVelocityX(0);
-      player.anims.play('turn');
+    player.setVelocityX(160);
+    player.anims.play('right', true);
+    communicate();
+  } else {
+    player.setVelocityX(0);
+    player.anims.play('turn');
   }
 
-  if (cursors.up.isDown && player.body.touching.down) {
-      player.setVelocityY(-330);
+  if (cursors.up.isDown && cursors.down.isDown) {
+    player.setVelocityY(0);
+  } else if (cursors.up.isDown) {
+    player.setVelocityY(-160);
+    communicate();
+  } else if (cursors.down.isDown) {
+    player.setVelocityY(160);
+    communicate();
+  } else {
+    player.setVelocityY(0);
   }
 }
 
